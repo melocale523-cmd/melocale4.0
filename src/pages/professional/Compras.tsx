7abc2e7f86f396a7c9eb00434bf3693ed8079e29
me locale <melocale523@gmail.com>
2026-05-01 15:55:39 -0300

    Update Compras.tsx

--- src/pages/professional/Compras.tsx
+++ src/pages/professional/Compras.tsx
@@ -9,7 +9,7 @@ export default function ProfessionalCompras() {
   const queryClient = useQueryClient()
   const [selectedPurchase, setSelectedPurchase] = useState<any | null>(null);
   const [isProposalModalOpen, setIsProposalModalOpen] = useState(false);
-  const [proposalData, setProposalData] = useState({h
+  , setProposalData] = useState({
     price: '',
     duration: '',
     description: '',
